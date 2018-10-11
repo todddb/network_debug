be2b8a391879c8bedcc7eec905cb97374ac8feef
todddb <todd_berrett@byu.edu>
2018-10-11 16:19:13 -0600

    Initial Commit
    This is my first commit

--- network_debug.ino
+++ network_debug.ino
@@ -12,8 +12,8 @@ void setup() {
   Serial.print("Connecting to '");
   Serial.print(ssid);
   Serial.println("' network");
-  Serial.print("MAC: ");
-  Serial.println(WiFi.macAddress());
+//  Serial.print("MAC: ");
+//  Serial.println(WiFi.macAddress());
   
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.hostname("todddb-arduino");
@@ -27,14 +27,18 @@ void setup() {
   Serial.println("WiFi connected");
  
   // Print the IP address
-  Serial.print("IP address: ");
-  Serial.print(WiFi.localIP());
+//  Serial.print("IP address: ");
+//  Serial.print(WiFi.localIP());
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rssi = WiFi.RSSI();
-  Serial.println(rssi);
-  delay(1000);
+  Serial.print("IP Address: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(", MAC Address: ");
+  Serial.print(WiFi.macAddress());
+  Serial.print(". RSSI: ");
+  Serial.println(WiFi.RSSI());
+  delay(2000);
 }
